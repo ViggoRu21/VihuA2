7abc64fdc892af64c1495fb455c7d7cc9eb29954
Glasabarn <viggoh21@ru.is>
2025-02-09 22:10:44 +0000

    Update dateUtils

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -5,18 +5,18 @@ export function getCurrentYear() {
   return moment().year();
 }
 
-export function add(date, number, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: moment.MomentInput, number: moment.DurationInputArg1, type = DATE_UNIT_TYPES.DAYS) {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date, from, to) {
+export function isWithinRange(date: moment.MomentInput, from: moment.MomentInput, to: moment.MomentInput) {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date, compareDate) {
+export function isDateBefore(date: moment.MomentInput, compareDate: moment.MomentInput) {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date, compareDate) {
+export function isSameDay(date: moment.MomentInput, compareDate: moment.MomentInput) {
   return moment(date).isSame(compareDate);
 }
